e5ed3bf3f89c744e2f26cc7650f7c36b915d134c
Dhruv <>
2021-04-17 14:35:22 +0000

    .

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests as r
 import datetime
 import math
 import keep_alive
+
 my_secret = os.environ['TOKEN']
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='>',
@@ -13,7 +14,6 @@ client = commands.Bot(command_prefix='>',
 
 
 @client.event
-@commands.cooldown(60 * 60, 30 * 30, commands.BucketType.user)
 async def on_member_join(member):
 
     while True:
@@ -31,29 +31,15 @@ async def on_member_join(member):
         await member.send(embed=embed)
         msg = await client.wait_for("message")
         if msg.content == captcha_answer:
-            await member.send("We got To know you Are Human , Thats Great . Now You Will be Able To Talk in Server. Have A Pleasant Stay")
+            await member.send(
+                "We got To know you Are Human , Thats Great . Now You Will be Able To Talk in Server. Have A Pleasant Stay"
+            )
             await member.add_roles(verified, reason="None")
             break
         else:
             await member.send("\n\n__Try Again__\n\n")
             pass
 
-
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        # await ctx.send(f"{error} ")
-        cooldown_time = str(
-            datetime.timedelta(seconds=math.trunc(error.retry_after)))
-        if cooldown_time == "0:00:00":
-            await ctx.send("ik you are too desperate wait for 1 sec atleast")
-        elif cooldown_time == "0:00:01":
-            await ctx.send("ik you are too desperate wait for 1 sec atleast")
-        else:
-            await ctx.send(
-                f"Fam You Are On Cooldown . Wait For like  {cooldown_time}")
-    else:
-        raise error
 
 keep_alive.keep_alive()
 client.run(my_secret)
